15c02791b8e00d7bebde39f01710d5edb2518167
Daniel Happacher <daniel@miamibluerecords.com>
2022-09-03 22:36:36 +0200

    no message

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -33,7 +33,7 @@ module.exports = {
             const voiceChannel = await guild.channels.create({
                 name: newChannelName,
                 type: ChannelType.GuildVoice,
-                bitrate: '256',
+                bitrate: '25600',
                 parent: newChannel.parent,
                 permissionOverwrites: [
                     {
