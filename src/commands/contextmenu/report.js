05cdc943610db47759c57db16f8bab02312147d6
Daniel Happacher <daniel@miamibluerecords.com>
2022-09-07 15:30:09 +0200

    add report to context menu

---
+++ src/commands/contextmenu/report.js
@@ -0,0 +1,45 @@
+const {
+    ContextMenuCommandBuilder,
+    ApplicationCommandType,
+    PermissionFlagsBits,
+  } = require("discord.js");
+  
+  module.exports = {
+    data: new ContextMenuCommandBuilder()
+      .setName("User Report")
+      .setType(ApplicationCommandType.User)
+      .setDefaultMemberPermissions(PermissionFlagsBits.ViewChannel),
+    async execute(interaction, client) {
+        const { options } = interaction;
+        const member = options.getMember("user");
+    
+        const modal = new ModalBuilder()
+          .setCustomId("userReport")
+          .setTitle(`User ${member.user.tag} melden!`);
+    
+        const textInput = new TextInputBuilder()
+          .setCustomId("reportUserInput")
+          .setLabel("Warum möchtest du den User melden?")
+          .setRequired(true)
+          .setStyle(TextInputStyle.Paragraph);
+    
+        const reportedUserInput = new TextInputBuilder()
+          .setCustomId("reportedUserInput")
+          .setLabel("User der gemeldet wird:")
+          .setValue(`${member.user.tag}`)
+          .setRequired(true)
+          .setStyle(TextInputStyle.Short);
+    
+        modal.addComponents(
+          new ActionRowBuilder().addComponents(textInput),
+          new ActionRowBuilder().addComponents(reportedUserInput)
+        );
+    
+        const commandLogRepository = require("../../mysql/commandLogRepository");
+                                              // guild - command, user, affectedMember, reason
+        await commandLogRepository.logCommandUse(interaction.guild, "report", interaction.user, "-", "-")
+    
+        await interaction.showModal(modal);
+    },
+  };
+  
